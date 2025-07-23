67abe39f48cb53aabcfcb767f6dcdc91caa47f2f
Roshan <neelamsairoshan2003@gmail.com>
2025-07-23 17:06:59 +0530

    add NotFound page with centered Google-hosted 404 image

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,14 +1,20 @@
-import { Component } from "react";
+import { Component } from "react"
+import "./index.css"
 
-class NotFound extends Component{
-    render(){
-        return(
-            <div>
-                <h1>
-                    NotFound
-                </h1>
-            </div>
-        )
-    }
+class NotFound extends Component {
+  render() {
+    return (
+      <div className="not-found-container">
+        <img
+          src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif"
+          alt="not found"
+          className="not-found-image"
+        />
+        <h1 className="not-found-heading">Oops! Page Not Found</h1>
+        <p className="not-found-text">We can't seem to find the page you're looking for.</p>
+      </div>
+    )
+  }
 }
-export default NotFound;
+
+export default NotFound
