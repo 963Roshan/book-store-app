930e8565eee2460eb41ecb42efd2d336ff0ebfa0
Roshan <neelamsairoshan2003@gmail.com>
2025-07-23 11:48:40 +0530

    Make BookItems in BookList clickable links to BookDetails using React Router

--- src/components/BookList/index.js
+++ src/components/BookList/index.js
@@ -1,11 +1,34 @@
 import { Component } from "react";
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
+import Header from "../Header";
 
 class BookList extends Component{
     render(){
         return(
-            <div>
-                <h1>BookList</h1>
-            </div>
+            <>
+            <Header/>
+        <div className="price-range-container">
+        <label className="price-label">Filter by Price</label>
+        <Slider
+          min={0}
+          max={100}
+          step={5}
+          value={priceRangeValue}
+          onChange={onChangePrice}
+        />
+        <p className="price-value">Up to ₹{priceRangeValue}</p>
+      </div>
+      <div>
+         <input
+        type="search"
+        className="search-input"
+        placeholder="Search by title or author"
+        value={searchInputValue}
+        onChange={onChangeSearchInput}
+      />
+      </div>
+            </>
         )
     }
 }
